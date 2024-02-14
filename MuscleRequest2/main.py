c4d7ec83a1410aa818a4a3356f79a3a16f2b12e9
Alex Walbom <alexwalbom@gmail.com>
2024-02-14 11:13:53 -0700

    files now delete at the beginning of the program

--- MuscleRequest2/main.py
+++ MuscleRequest2/main.py
@@ -15,12 +15,9 @@ def checkStatus(jobID):
             keepChecking = False
             
 def get_uniprot_entry(entry_id):
-    # either of the below URLs will work
-    # url = f'https://rest.uniprot.org/uniprotkb/search?query={entry_id}&format=fasta'
-    # url = f'https://rest.uniprot.org/uniprotkb/{entry_id}.fasta'
     url = f'https://www.ebi.ac.uk/proteins/api/proteins/{entry_id}'
 
-    response = requests.get(url)
+    response = requests.get(url, headers={ "Accept" : "text/x-flatfile"})
 
     if response.status_code == 200:
         return response.text
@@ -43,7 +40,23 @@ def readDirectoryContents():
 
     return sequences
 
+def remove_files_in_subfolder(folder_path):
+    # Get the list of files in the subfolder
+    files_to_remove = os.listdir(folder_path)
+    if os.path.exists(os.path.join(os.getcwd(), "alignment.clustal")):
+        os.unlink(os.path.join(os.getcwd(), "alignment.clustal"))
+
+    # Iterate through the files and remove them
+    for file_name in files_to_remove:
+        file_path = os.path.join(folder_path, file_name)
+        try:
+            os.unlink(file_path)
+        except Exception as e:
+            print(f"Error removing {file_path}: {e}")
+
 if __name__ == "__main__":
+    folder_path = os.path.join(os.getcwd(), "uniprotEntries")
+    remove_files_in_subfolder(folder_path)
     uniprot_ids = ['P37869', 'A5FUW3', 'A0A411SXA6', 'A0A0P7YND4', 'B6W7V0', 'Q815K8', 'B7GTK2', 'A0A0A7K4W5', 'A0A411DND6', 'C0B602', 'G0EYL2', 'A0A080NRS1', 'A0A1H1MXD9', 'C9RQM6',
                'A0Q5J9', 'A0A0P8B8C6', 'Q5ZTX1', 'A0A8B4R093', 'D5ES25', 'Q88MF9', 'Q0S4I1', 'Q6N5U6', 'A0A0P7VUY9', 'A7AZX6', 'Q08YA3', 'F2R6D4', 'Q31QJ8']
 
@@ -76,15 +89,16 @@ if __name__ == "__main__":
 
     sequences = readDirectoryContents()
 
-    with open(os.path.join(os.getcwd, "uniprotEntries"), 'w') as myFile:
+    with open(os.path.join(os.getcwd(), "uniprotEntries", "sequenceFile.txt"), 'w') as myFile:
         myFile.write(sequences)
 
-        myFile.close(sequences)
+        myFile.close()
 
     data = {
         "email": email,
         "title": title,
-        "sequence": sequences
+        "sequence": sequences,
+        "tree": "tree1"
     }
 
     responseCode = requests.post(postURL, data=data)
@@ -94,17 +108,15 @@ if __name__ == "__main__":
 
     status = checkStatus(jobID)
 
-    # resultType = requests.get(getURL1)
-    # print(resultType)
-
     format = "aln-clustalw"
 
     getURL2 = f"https://www.ebi.ac.uk/Tools/services/rest/muscle/result/{jobID}/{format}"
     alignment = requests.get(getURL2)
-    print(alignment.text)
     f = open("alignment.clustal", "a")
     f.write(alignment.text)
     f.close()
+
+
 
 
     # Check the status code and handle the response
