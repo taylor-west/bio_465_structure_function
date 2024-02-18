2e8a0abbf9ee6387dfc7f10f2c6cd709700fc78f
Alex Walbom <alexwalbom@gmail.com>
2024-02-18 16:50:44 -0700

    reworked the Nodestyles. colors work now

--- phylogenetic_tree/phylo_tree.py
+++ phylogenetic_tree/phylo_tree.py
@@ -20,22 +20,39 @@ def make_tree_with_species_names():
     for leaf in tree.iter_leaves():
         leaf.name = idDict[leaf.name]
 
+    # Traverse the tree and apply the default background color function
+
     ts = ete3.TreeStyle()
     ts.show_leaf_name = False
     ts.show_branch_length = False
     ts.force_topology = False
+    ts.layout_fn = my_layout
+    ts.allow_face_overlap = False
+    set_styles_for_each_group(tree)
+
+    # Render the tree to a file (e.g., tree.png)
+    tree.show()
+    # tree.render("tree.png", tree_style=ts)
+
+def set_styles_for_each_group(tree):
     # Hominidae
     h_style = NodeStyle()
-    h_style["bgcolor"] = "#9ac4f8"
+    h_style["bgcolor"] = "lightblue"
     Hominidae_list = ["Homo_sapiens", "Pan_troglodytes", "Gorilla_gorilla"]
     Hominidae_root = tree.get_common_ancestor(Hominidae_list)
     Hominidae_root.set_style(h_style)
 
+
     # Cercopithecidae
     c_style = NodeStyle()
     c_style["bgcolor"] = "#c3a1f7"
-    Cercopithecidae_list = ["Papio_anubis", "Macaca_nemestrina",
-                            "Colobus_angolensis", "Macaca_mulatta"]
+    Cercopithecidae_list = ["Macaca_nemestrina", "Macaca_mulatta"]
+    colobus = "Colobus_angolensis"
+    papio = "Papio_anubis"
+    col_node = tree.search_nodes(name=colobus)[0]
+    pap_node = tree.search_nodes(name=papio)[0]
+    col_node.set_style(c_style)
+    pap_node.set_style(c_style)
     Cercopithecidae_root = tree.get_common_ancestor(Cercopithecidae_list)
     Cercopithecidae_root.set_style(c_style)
 
@@ -81,16 +98,19 @@ def make_tree_with_species_names():
     Muridae_root = tree.get_common_ancestor(Muridae_list)
     Muridae_root.set_style(m_style)
 
-    ts.layout_fn = my_layout
-    # Render the tree to a file (e.g., tree.png)
-    tree.show()
-    tree.render("tree.png", tree_style=ts)
-
+    # None
+    no_style = NodeStyle()
+    no_style["bgcolor"] = "white"
+    Reject_list = ["Carlito_syrichta", "Microcebus_murinus"]
+    for node_name in Reject_list:
+        node = tree.search_nodes(name=node_name)[0]
+        node.set_style(no_style)
 
 # Define a layout function to align leaf names to the right
 def my_layout(node):
+    faces.add_face_to_node(faces.RectFace(width=10, height=10, bgcolor="white"), node, column=0, aligned=True)
     if node.is_leaf():
-        faces.add_face_to_node(faces.TextFace(node.name, fsize=10, fgcolor="black", fstyle="italic"), node, column=0, position="branch-right")
+        faces.add_face_to_node(faces.TextFace(node.name, fsize=8, fgcolor="black", fstyle="italic"), node, column=0, position="branch-right")
 
 def get_species_names_and_uniprot_ids_from_uniprot_entries():
     currentDirectory = os.getcwd()
@@ -115,7 +135,7 @@ def get_species_names_and_uniprot_ids_from_uniprot_entries():
     with open(os.path.join(currentDirectory, "organisms.txt"), 'w') as outF:
                 i = 0
                 for organism in organisms:
-                    outF.write(str(ids[i]) +  " " + organism + "\n")
+                    outF.write(str(ids[i]) + " " + organism + "\n")
                     i += 1
 
 if __name__ == "__main__":
