811fd51095cb49e95b3cbcc56918dfe353926baf
Alex Walbom <alexwalbom@gmail.com>
2024-02-17 19:06:36 -0700

    shows a tree with colors

--- phylogenetic_tree/phylo_tree.py
+++ phylogenetic_tree/phylo_tree.py
@@ -1,5 +1,8 @@
 import os
-from ete3 import NodeStyle
+
+import ete3
+from ete3 import faces, NodeStyle
+
 
 def make_tree_with_species_names():
     idDict = {}
@@ -16,6 +19,78 @@ def make_tree_with_species_names():
     #change nodes to be labeled with species names instead of protein IDs
     for leaf in tree.iter_leaves():
         leaf.name = idDict[leaf.name]
+
+    ts = ete3.TreeStyle()
+    ts.show_leaf_name = False
+    ts.show_branch_length = False
+    ts.force_topology = False
+    # Hominidae
+    h_style = NodeStyle()
+    h_style["bgcolor"] = "#9ac4f8"
+    Hominidae_list = ["Homo_sapiens", "Pan_troglodytes", "Gorilla_gorilla"]
+    Hominidae_root = tree.get_common_ancestor(Hominidae_list)
+    Hominidae_root.set_style(h_style)
+
+    # Cercopithecidae
+    c_style = NodeStyle()
+    c_style["bgcolor"] = "#c3a1f7"
+    Cercopithecidae_list = ["Papio_anubis", "Macaca_nemestrina",
+                            "Colobus_angolensis", "Macaca_mulatta"]
+    Cercopithecidae_root = tree.get_common_ancestor(Cercopithecidae_list)
+    Cercopithecidae_root.set_style(c_style)
+
+    # Tarsiidae
+    t_style = NodeStyle()
+    t_style["bgcolor"] = "#FFFFFF"
+    Tarsiidae_list = ["Carlito_syrichta", "Microcebus_murinus"]
+    Tarsiidae_root = tree.get_common_ancestor(Tarsiidae_list)
+    Tarsiidae_root.set_style(t_style)
+
+    # Cebidae
+    ceb_style = NodeStyle()
+    ceb_style["bgcolor"] = "#a2e57b"
+    Cebidae_list = ["Callithrix_jacchus", "Saimiri_boliviensis"]
+    Cebidae_root = tree.get_common_ancestor(Cebidae_list)
+    Cebidae_root.set_style(ceb_style)
+
+    # Heteromyidae
+    #het_style = NodeStyle()
+    #het_style["bgcolor"] = "#ffb3b3"
+    #Heteromyidae_list = ["Dipodomys_ordii"]
+    #Heteromyidae_root = tree.get_common_ancestor(Heteromyidae_list)
+    #Heteromyidae_root.set_style(het_style)
+
+    # Sciuridae
+    s_style = NodeStyle()
+    s_style["bgcolor"] = "#fff59d"
+    Sciuridae_list = ["Heterocephalus_glaber", "Castor_canadensis","Dipodomys_ordii"]
+    Sciuridae_root = tree.get_common_ancestor(Sciuridae_list)
+    Sciuridae_root.set_style(s_style)
+
+    # Cheirogaleidae
+    #Che_style = NodeStyle()
+    #Che_style["bgcolor"] = "#ffcc80"
+    #Cheirogaleidae_list = ["Microcebus_murinus"]
+    #Cheirogaleidae_root = tree.get_common_ancestor(Cheirogaleidae_list)
+    #Cheirogaleidae_root.set_style(Che_style)
+
+    # Muridae
+    m_style = NodeStyle()
+    m_style["bgcolor"] = "#ffcc80"
+    Muridae_list = ["Rattus_norvegicus", "Mus_musculus", "Mus_caroli", "Cricetulus_griseus", "Nannospalax_galili", "Mesocricetus_auratus"]
+    Muridae_root = tree.get_common_ancestor(Muridae_list)
+    Muridae_root.set_style(m_style)
+
+    ts.layout_fn = my_layout
+    # Render the tree to a file (e.g., tree.png)
+    tree.show()
+    tree.render("tree.png", tree_style=ts)
+
+
+# Define a layout function to align leaf names to the right
+def my_layout(node):
+    if node.is_leaf():
+        faces.add_face_to_node(faces.TextFace(node.name, fsize=10, fgcolor="black", fstyle="italic"), node, column=0, position="branch-right")
 
 def get_species_names_and_uniprot_ids_from_uniprot_entries():
     currentDirectory = os.getcwd()
