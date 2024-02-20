0ff43c867fb167c083ec9a9789698a0ed2efd6f1
Taylor West <west.taylor.h@gmail.com>
2024-02-20 10:36:19 -0700

    (dev) partial modification to limit script to specific pathways and orthologs

--- ortholog_identification/kegg_orthologs.py
+++ ortholog_identification/kegg_orthologs.py
@@ -1,77 +1,100 @@
 import requests
 import re
 import os
-# import json
 import pandas as pd
 from Bio.KEGG import REST
 
-PATHWAY_KEYWORDS = ["metabolic", "metabolism"]
-CURR_DIRECTORY_PATH = os.path.dirname(__file__)
-READ_PATHWAY_FROM_FILE = True
+READ_PATHWAY_FROM_FILE = False
 READ_ORTHOLOGS_FROM_FILE = True
-READ_GENES_FROM_FILE = False
+READ_GENES_FROM_FILE = True
+USING_TEMP_GENE_DATA = True
+PATHWAY_KEYWORDS = ["metabolic", "metabolism"]
+
+CURR_DIRECTORY_PATH = os.path.dirname(__file__)
+TARGET_PATHWAYS_FILENAME = 'target_pathways.csv'
+PATHWAY_ORTHOLOGS_FILENAME = 'pathway_orthologs.csv'
+ORTHOLOG_GENES_FILENAME = 'ortholog_genes.csv'
+TEMP_GENE_FILENAME = 'temp_gene_data.csv'
+UNIPROT_IDS_FILENAME = 'uniprot_ids.csv'
 
 # Step 1: get a list of the correct pathways for humans
-# Step 2: find all genes/entries associated with those pathways
-# Step 3: get KEGG ortholog entry from gene
-# Step 4: get all genes associated with that ortholog, making sure to track organisms
+# Step 2: get all KEGG ortholog entries associated with each pathway
+# Step 3: get all genes associated with each ortholog
+# Step 4: get UniProt IDs for each gene
 
 def get_uniprot_ids_for_pathways():
     print('in kegg_orthologs.get_uniprot_ids_for_pathways...')
 
     # get pathways data
     if READ_PATHWAY_FROM_FILE:
-       pathways_df = pd.read_csv(os.path.join(CURR_DIRECTORY_PATH, 'data', 'target_pathways.csv')) 
+       pathways_df = pd.read_csv(os.path.join(CURR_DIRECTORY_PATH, 'data', 'working', TARGET_PATHWAYS_FILENAME)) 
        
     else:
         pathways = get_pathways()
         pathways_df = pd.DataFrame(pathways, columns=['pathway_name', 'pathway_description'])
 
         # write pathways to a file (FOR TESTING/DEV)
-        pathways_df.to_csv(os.path.join(CURR_DIRECTORY_PATH, 'data', 'target_pathways.csv'))
+        pathways_df.to_csv(os.path.join(CURR_DIRECTORY_PATH, 'data', TARGET_PATHWAYS_FILENAME))
 
     # get orthologs for each pathway
     if READ_ORTHOLOGS_FROM_FILE:
-       pathway_orthologs_df = pd.read_csv(os.path.join(CURR_DIRECTORY_PATH, 'data', 'pathway_orthologs.csv')) 
+       pathway_orthologs_df = pd.read_csv(os.path.join(CURR_DIRECTORY_PATH, 'data', 'working', PATHWAY_ORTHOLOGS_FILENAME)) 
        
     else:
         kegg_ids_list = []
         for index, row in pathways_df.iterrows():
             kegg_ids = get_orthologs_by_pathway(row['pathway_name'])
             kegg_ids_list.append(kegg_ids)
-        pathway_orthologs_df = pathways_df.assign(ko_id=kegg_ids_list).explode('ko_id').dropna(subset=["ko_id"])       
+        pathway_orthologs_df = pathways_df.assign(ko_id=kegg_ids_list).explode('ko_id').dropna(subset=["ko_id"]).drop_duplicates()       
 
         # write orthologs to a file (FOR TESTING/DEV)
-        pathway_orthologs_df.to_csv(os.path.join(CURR_DIRECTORY_PATH, 'data', 'pathway_orthologs.csv'))
+        pathway_orthologs_df.to_csv(os.path.join(CURR_DIRECTORY_PATH, 'data', 'working', PATHWAY_ORTHOLOGS_FILENAME))
 
 
 
 
     # get genes for each KEGG ortholog
     if READ_GENES_FROM_FILE:
-        ortholog_genes_df = pd.read_csv(os.path.join(CURR_DIRECTORY_PATH, 'data', 'ortholog_genes.csv')) 
+        ortholog_genes_df = pd.read_csv(os.path.join(CURR_DIRECTORY_PATH, 'data', 'working', ORTHOLOG_GENES_FILENAME)) 
     else:
         # ortholog_genes_df = df = pathway_orthologs_df.reindex(columns=["pathway_name", 'pathway_description', "ko_id", "gene_name", "gene_organism"])
         gene_list = []
+        temp_gene_df = (pd.DataFrame(columns=['ko_id', 'gene_name']) if USING_TEMP_GENE_DATA else pd.read_csv(os.path.join(CURR_DIRECTORY_PATH, 'data', TEMP_GENE_FILENAME)))
+        temp_gene_df
         for index, row in pathway_orthologs_df.iterrows():
-            genes = get_genes_by_ortholog(row["ko_id"]) # gets an array of the gene names
-            gene_list.append(genes)
-            # ortholog_genes_df.merge(genes, on='ko_id', how='left') # old way using a merge, but is super slow
+            if not (USING_TEMP_GENE_DATA and (row["ko_id"] in temp_gene_df['ko_id'])):
+                genes = get_genes_by_ortholog(row["ko_id"], pathway_orthologs_df) # gets an array of the gene names
+                gene_list.append(genes)
+
+                # TODO: remove this after testing
+                temp_df = pd.DataFrame([{'ko_id': row["ko_id"], 'gene_name': gene} for gene in genes], columns=['ko_id', 'gene_name'])
+                temp_gene_df = pd.concat([temp_gene_df, temp_df])
+                temp_gene_df.to_csv(os.path.join(CURR_DIRECTORY_PATH, 'data', 'temp', 'working', TEMP_GENE_FILENAME))
+                ####
+
         ortholog_genes_df = pathway_orthologs_df.assign(gene_name=gene_list).explode('gene_name').dropna(subset=["gene_name"])       
         
         #write genes to a file
-        ortholog_genes_df.to_csv(os.path.join(CURR_DIRECTORY_PATH, 'data', 'ortholog_genes.csv'))
+        ortholog_genes_df.to_csv(os.path.join(CURR_DIRECTORY_PATH, 'data', ORTHOLOG_GENES_FILENAME))
 
 
     # get uniprot ids for each KEGG ortholog
-    uniprot_ids = []
-    for index, row in ortholog_genes_df.iloc[:, "gene_name"]:
-        uniprot_ids.append(get_uniprot_ids_by_gene(row["gene_name"]))
-    uniprot_ids_df = ortholog_genes_df.assign(uniprot_id=uniprot_ids)
-    
+    # uniprot_ids_df = ortholog_genes_df.reindex(columns = ortholog_genes_df.columns.tolist() + ['uniprot_id'])
+    id_list = []
+    for index, row in ortholog_genes_df.iterrows():
+        uniprot_id = get_uniprot_ids_by_gene(row["gene_name"])
+        id_list.append(uniprot_id)
+        # row['uniprot_id']=uniprot_id
+
+        # TODO: remove this after testing
+        # target_organisms = ['hsa', 'ptr', 'pps', 'ggo', 'pon', 'nle', 'hmh', 'mcc']
+        # temp_df = pd.DataFrame([{'ko_id': row["ko_id"], 'gene_name': gene} for gene in genes], columns=['ko_id', 'gene_name'])
+        # temp_gene_df = pd.concat([temp_gene_df, temp_df])
+        # temp_gene_df.to_csv(os.path.join(CURR_DIRECTORY_PATH, 'data', 'temp', TEMP_GENE_FILENAME))
+    uniprot_ids_df = ortholog_genes_df.assign(uniprot_ids=id_list).dropna(subset=["uniprot_ids"])       
 
     #write uniprot ids to a file
-    uniprot_ids_df.to_csv(os.path.join(CURR_DIRECTORY_PATH, 'data', 'uniprot_ids.csv'))
+    uniprot_ids_df.to_csv(os.path.join(CURR_DIRECTORY_PATH, 'data', 'working', UNIPROT_IDS_FILENAME))
     
     ortholog_data = []
     return ortholog_data
@@ -101,19 +124,20 @@ def get_pathways():
 
 
 # gets all the orthologs from this KEGG ID
-def get_genes_by_ortholog(kegg_ortholog_id):
+def get_genes_by_ortholog(kegg_ortholog_id, temp_save_df):
     print(f'in kegg_orthologs.get_genes_by_ortholog for kegg_ortholog_id={kegg_ortholog_id}...')
 
     ortholog_genes_response = REST.kegg_find("genes", kegg_ortholog_id).read().rstrip().split('\n')
     
-    # This is another option if we need to include gene description data in the final dataframe. It is significantly slower though.
-    # ortholog_genes_response_df = pd.DataFrame([line.split("\t") for line in ortholog_genes_response], columns=['gene_name', 'gene_description'])
-
+    # # This is another option if we need to include gene description data in the final dataframe. It is significantly slower though.
+    # ortholog_genes_response_df = pd.DataFrame([kegg_ortholog_id, [line.split("\t")[0] for line in ortholog_genes_response]], columns=['ko_id', 'gene_name'])
+    # temp_save_df.append(ortholog_genes_response_df)
+    # temp_save_df.to_csv(os.path.join(CURR_DIRECTORY_PATH, 'data', 'temp', 'temp_gene_data.csv'))
+    
     gene_names = [line.split("\t")[0] for line in ortholog_genes_response]
     return gene_names
 
 def get_orthologs_by_pathway(pathway_name):
-    # https://www.biostars.org/p/6224/#6355
     print(f'in kegg_orthologs.get_genes_by_pathway for pathway_name={pathway_name}...')
 
     url = f'https://rest.kegg.jp/get/{pathway_name}'
@@ -122,7 +146,6 @@ def get_orthologs_by_pathway(pathway_name):
 
     if response.status_code == 200:
         pathway_data = response.text
-        # print(pathway_data)
     else:
         return None
 
@@ -164,13 +187,17 @@ def get_orthologs_by_pathway(pathway_name):
 def get_uniprot_ids_by_gene(gene_name):
     print('in kegg_orthologs.get_uniprot_ids_by_gene...')
     gene_response = REST.kegg_get(gene_name).read()
-    # TODO: extract uniprot_id from KEGG gene response
-    gene_response
-    return
+    # Regular expression pattern to match UniProt ID
+    pattern = r'UniProt:\s+(\S+)'
+    # Extracting UniProt ID using regular expression
+    match = re.search(pattern, gene_response)
+    if match:
+        uniprot_id = match.group(1)
+        print("UniProt ID:", uniprot_id)
+    else:
+        uniprot_id = None
+        print("UniProt ID not found.")
+    return uniprot_id
 
 if __name__ == "__main__":
     get_uniprot_ids_for_pathways()
-
-
-# Exactly what we want to do, but it is in R instead of Python
-# https://support.bioconductor.org/p/109871/
