aa0df17f6539107be44861f96fd68982f4835ea4
Taylor West <west.taylor.h@gmail.com>
2024-03-01 14:45:19 -0700

    (dev) added minimal modifications to structure_function_pipeline for new orthologs parameters

--- structure_function_pipeline.py
+++ structure_function_pipeline.py
@@ -7,7 +7,7 @@ HelpOutput = """
 Required Parameters:
 -k  <file>  This is a csv file containing a list of keywords that will be used to filter pathways in KEGG.
 
--p  <file>  This is a csv file containing the pathways that will be used to retrieve orthologs in KEGG.
+-p  <file>  This is a string naming the KEGG pathway ID that will be used to retrieve orthologs in KEGG.
 
 -o   <file>  This is a csv file containing the KEGG orthologs corresponding to the genes whose uniprot_id's will be retrieved.
 -g   <file>  This is a csv file of genes for which to get the uniprot_id's.
