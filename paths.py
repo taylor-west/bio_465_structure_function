325a00351fc6cd7f272782d72bcc81a898870b9e
Alex Walbom <alexwalbom@gmail.com>
2024-03-08 13:53:54 -0700

    merged

--- paths.py
+++ paths.py
@@ -2,6 +2,7 @@ import os
 
 CWD = os.getcwd()
 PATH_TO_DATAFILES = os.path.join(CWD, "../datafiles")
+PATH_TO_DATAFILES_FROM_MAIN = "datafiles" #check this one idk. This all might need to be refined...
 PATH_TO_FIGURE1 = os.path.join(PATH_TO_DATAFILES, "figure1")
 PATH_TO_MUSCLE_DATA = os.path.join(PATH_TO_DATAFILES, "muscle_data")
 PATH_TO_UNIPROT_ENTRIES = os.path.join(PATH_TO_DATAFILES, "uniprot_entries")
