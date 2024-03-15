a6d052a814e1b32f29b9d53675c59d3ec62f7654
Alex Walbom <alexwalbom@gmail.com>
2024-03-15 16:16:20 -0600

    eval file successfully generates expected residue lists

--- eval/eval.py
+++ eval/eval.py
@@ -1,5 +1,11 @@
 import pandas as pd
 import os
+import json
+import requests
+from urllib.request import urlretrieve
+
+from Bio.PDB import PDBParser
+
 from cluster_analysis_3d.CalculateResidueDistanceWithDataframeInput import find_clusters, filter_interesting_clusters, find_common_clusters
 
 CWD = os.getcwd()
@@ -39,7 +45,7 @@ def get_key_rows_only(dataframe):
         act_positions = key_positions.get('ACT_SITE', [])
         bind_positions = key_positions.get('BINDING', [])
         all_key_positions = act_positions + bind_positions
-        # only_key_rows_df = group.loc[group['seq_pos'].isin(all_key_positions), 'seq_pos']
+        # makes a dataframe that only includes the rows with key positions
         only_key_rows_df = group[group.seq_pos.isin(all_key_positions)].copy()
 
         # is_key = []
@@ -54,6 +60,108 @@ def get_key_rows_only(dataframe):
         only_key_rows_df['key_positions'] = key_positions_list
         aggregated_df = pd.concat([aggregated_df, only_key_rows_df], ignore_index=True)
     return aggregated_df
+
+def make_expected_cluster_lists_for_all_invariant_residues(invariant_res_df: pd.DataFrame, distance_threshold: float):
+
+        residue_codes = pd.read_csv('../datafiles/cluster_data/residue_codes.csv')
+
+        filepath = '../datafiles/pdb_files/'
+        os.makedirs(filepath, exist_ok=True)
+
+        uniprot_ids = set(invariant_res_df['uniprot_id'])
+        cluster_col = [None for i in range(len(invariant_res_df))]
+        invariant_res_df['clusters'] = cluster_col
+        invariant_res_df['expected_residues'] = None
+        # going through every uniprot id in our dataframe
+        grouped_by_uniprot = invariant_res_df.groupby('uniprot_id')
+        for uniprot_id, group in grouped_by_uniprot:
+            filtered_by_id_df = invariant_res_df[invariant_res_df['uniprot_id'] == uniprot_id]
+            entry_id = filtered_by_id_df['entry_id'].iloc[0]
+            uniprot_filename = entry_id + ".txt"
+            key_positions = read_uniprot_file_to_analyze_active_sites(PATH_TO_UNIPROT_ENTRIES, uniprot_filename)
+            # Store key_positions in a list
+            act_positions = key_positions.get('ACT_SITE', [])
+            bind_positions = key_positions.get('BINDING', [])
+            all_key_positions = act_positions + bind_positions
+
+            clusters_dict = {}
+            url = f'https://alphafold.ebi.ac.uk/api/prediction/{uniprot_id}'
+
+            response = requests.get(url)
+
+            alphafold_data = None
+
+            if response.status_code == 200:
+                alphafold_data = response.text
+            else:
+                print(f'Error: Unable to retrieve AlphaFold entry for {uniprot_id}')
+
+            if alphafold_data:
+                # get the pdb url to download the file
+                alphafold_json = json.loads(alphafold_data)
+                pdb_url = alphafold_json[0]['pdbUrl']
+                print(pdb_url)
+
+                filename = f'alphafold_{uniprot_id}.pdb'
+                urlretrieve(pdb_url, filepath + filename)
+
+                if os.path.exists(filepath + filename):
+
+                    # create parser
+                    parser = PDBParser()
+
+                    # read structure from file
+                    # TODO: update the id in this next line
+                    structure = parser.get_structure('ENOLASE (A0A2P1UMH5)', filepath + filename)
+
+                    # set up empty dictionary to track clusters
+                    locs_3d_dict = {}
+                    model = structure[0]
+                    chain = model['A']
+
+                    # create a dictionary to hold the 3d positions of every uniprot residue
+                    uniprot_residue_3d_positions = {}
+                    for position in all_key_positions:
+                        residue = chain[position]
+                        uniprot_residue_3d_positions[position] = residue['CA']
+
+
+                    for i, row in filtered_by_id_df.iterrows():
+                        residue = chain[row['seq_pos']]
+                        csv_map_row = residue_codes[residue_codes['Three Letter Code'] == residue.get_resname()]
+                        single_letter_code = list(csv_map_row['Single Letter Code'])[0]
+
+                        if single_letter_code == row['residue']:
+                            locs_3d_dict[row['seq_pos']] = residue['CA']
+
+                    for pos in locs_3d_dict:
+                        clusters_list = []
+                        expected_residues_list = []
+                        # calculate distance between current invariant residue and all key residues for this protein
+                        for uniprot_residue, position_3d in uniprot_residue_3d_positions.items():
+                            distance = locs_3d_dict[pos] - position_3d
+                            if distance <= distance_threshold:
+                                if distance != 0:
+                                    expected_residues_list.append(uniprot_residue)
+
+                        for pos2 in locs_3d_dict:
+                            if pos != pos2:
+                                # calculate distance between residues
+                                distance = locs_3d_dict[pos] - locs_3d_dict[pos2]
+                                if distance <= distance_threshold:
+                                    clusters_list.append(pos2)
+
+                        if len(clusters_list) > 0:
+                            index = invariant_res_df.loc[(invariant_res_df['uniprot_id'] == uniprot_id) & (
+                                        invariant_res_df['seq_pos'] == pos), 'clusters'].index[0]
+                            invariant_res_df.at[index, 'clusters'] = clusters_list
+                            invariant_res_df.at[index, 'expected_residues'] = expected_residues_list
+            for file in os.listdir(filepath):
+                os.remove(f'{filepath}{file}')
+        invariant_res_df = invariant_res_df.drop(columns=['MSA_sequence'])
+        invariant_res_df.to_csv('../datafiles/cluster_data/clusters.csv', index=False)
+        return invariant_res_df
+
 
 def analyze_key_locations(dataframe):
     key_results_df = get_key_rows_only(dataframe)
@@ -80,14 +188,16 @@ def evaluate_results(dataframe):
     results_df.to_csv(os.path.join(PATH_TO_EVAL_FILES, 'analyzed_clusters.csv'))
 
 invariant_res_df = pd.read_csv('../datafiles/muscle_data/MSA_results.csv')
-clusters_df = pd.read_csv('../datafiles/cluster_data/clusters.csv')
-invariant_res_df.drop(columns='Unnamed: 0', inplace=True)
+# clusters_df = pd.read_csv('../datafiles/cluster_data/clusters.csv')
+# invariant_res_df.drop(columns='Unnamed: 0', inplace=True)
 
-result = find_clusters(invariant_res_df, 10)
+# result = find_clusters(invariant_res_df, 10)
 
 #result2 = cluster.filter_interesting_clusters(clusters_df, 20)
-print(result.dtypes)
-evaluate_results(result)
+# print(result.dtypes)
+# evaluate_results(result)
+
+make_expected_cluster_lists_for_all_invariant_residues(invariant_res_df, distance_threshold=7)
 
 
 
